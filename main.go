725968ffe8d832049a32323c065e387f2f2d02f2
woremacx <woremacx@gmail.com>
2018-02-24 16:45:58 +0900

    slack token from .env

--- main.go
+++ main.go
@@ -3,16 +3,24 @@ package main
 import (
 	"context"
 	"flag"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/nlopes/slack"
 )
 
 func main() {
-	token := flag.String("token", "", "API token for slack")
+	godotenv.Load()
+
+	token := os.Getenv("API_TOKEN_FOR_SLACK")
+	if len(token) == 0 {
+		panic("require token on .env")
+	}
+
 	lang := flag.String("lang", "en", "Language to speak")
 	flag.Parse()
 
 	ctx := context.Background()
-	client := slack.New(*token)
+	client := slack.New(token)
 	NewSlackBot(client, *lang).Run(ctx)
 }
